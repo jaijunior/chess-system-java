af300abff7b82660b6cb1ae2eedba086f934d12e
aries <jaijunior11@gmail.com>
2023-03-23 20:59:11 -0300

    Possible moves of a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -45,6 +45,9 @@ public class ChessMatch {
 		if(!board.thereIsAPiece(position)) {
 			throw new ChessException("Theres no piece on souce position");
 		}
+		if (!board.piece(position).inThereAnyPossibleMove()) {
+			throw new ChessException("There's no possible moves for the chosen piece");
+		}
 	}
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
